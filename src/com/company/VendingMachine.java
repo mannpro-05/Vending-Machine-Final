7e4cb56fe2eca416e255ac39e82487b38f4bfac3
VIdhey Oza <vidhey97@gmail.com>
2021-09-20 22:29:52 -0400

    Initial Commit

--- src/com/company/VendingMachine.java
+++ src/com/company/VendingMachine.java
@@ -1,25 +1,28 @@
 package com.company;
 
 import static com.company.DrinkTypes.*;
+import com.company.DrinkTypes;
 
 public class VendingMachine extends Drink {
 
     private int totalMilk, totalSugar;
     DrinkTypes drinkType;
 
-    /*
+    /**
      * This Constructor will initialize the class variables.
      * It will also set the drink type.
-     * */
+     * @param drinkType DrinkType
+     */
     public VendingMachine(DrinkTypes drinkType) {
         this.totalMilk = 0;
         this.totalSugar = 0;
         this.drinkType = drinkType;
     }
-    /*
+
+    /**
      * This method will get the total milk units for the drink and will determine if more could be added.
      * If more could be added then it will add it and update the total Condiments in the drink.
-     * */
+     */
     @Override
     public void addMilk() {
         super.addMilk();
@@ -30,10 +33,11 @@ public class VendingMachine extends Drink {
             setTotalNumberOfCondiments(1);
         }
     }
-    /*
+
+    /**
      * This method will get the total sugar units in the drink and will determine if more could be added.
      * If more could be added then it will add it and update the total Condiments in the drink.
-     * */
+     */
     @Override
     public void addSugar() {
         super.addSugar();
@@ -44,31 +48,31 @@ public class VendingMachine extends Drink {
             setTotalNumberOfCondiments(1);
         }
     }
+
     /*
-    * This method contains the brewing process for all the drinks.
-    * */
-    public void brew(){
+     * This method contains the brewing process for all the drinks.
+     * */
+    public void brew() {
+        switch (this.drinkType) {
 
-        if(this.drinkType == AMERICANO)
-        {
-            System.out.println("Brewing Process of Americano");
+            case AMERICANO:
+                System.out.println("Brewing Process of Americano");
+                break;
+            case LATTE_MACCHIATO:
+                System.out.println("Brewing Process of Latte Macchiato");
+                break;
+            case ESPRESSO:
+                System.out.println("Brewing Process of Espresso");
+                break;
+            case BLACK_TEA:
+                System.out.println("Brewing Process of Black Tea");
+                break;
+            case GREEN_TEA:
+                System.out.println("Brewing Process of Green tea");
+                break;
+            case YELLOW_TEA:
+                System.out.println("Brewing Process of Yellow Tea");
+                break;
         }
-        else if(this.drinkType == ESPRESSO){
-            System.out.println("Brewing Process of Espresso");
-        }
-        else if(this.drinkType == LATTE_MACCHIATO){
-            System.out.println("Brewing Process of Latte Macchiato");
-        }
-        else if(this.drinkType == GREEN_TEA){
-            System.out.println("Brewing Process of Green tea");
-        }
-        else if(this.drinkType == YELLOW_TEA){
-            System.out.println("Brewing Process of Yellow Tea");
-        }
-        else{
-            System.out.println("Brewing Process of Black Tea");
-        }
-
     }
-
 }
